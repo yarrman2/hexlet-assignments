a9b37f967c218bca5dac575abf9ca7a77db6a0d9
@hexlet/cli <support@hexlet.io>
2024-03-29 15:45:41 +0300

    submit rails-basics-ru/models

---
+++ rails-basics-ru/models/app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    id = params[:id]
+    @article = Article.find(id)
+  end
+
+  def article_url
+    ''
+  end
+end
---
+++ rails-basics-ru/models/config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'home#index'
+  get '/articles', to: 'articles#index'
+  get '/articles/:id', to: 'articles#show'
+
+  resource :articles, only: :show
+
+end
